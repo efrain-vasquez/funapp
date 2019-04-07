c786f291a087424b1a6f1cc35e83ee6bed2c0b3f
efrain-vasquez <efrain.vasquez.arias777@gmail.com>
2019-04-06 23:27:58 -0600

    update to saturdayapp secondcomponent

--- public/react/components/first.jsx
+++ public/react/components/first.jsx
@@ -2,7 +2,7 @@ import React,{Component} from 'react';
 import axios from 'axios';
 
 
-export default class First extends React.Component {
+export default class First extends Component {
   constructor(props){
     super(props);
     this.state = {
--- public/react/components/second.jsx
+++ public/react/components/second.jsx
@@ -1,36 +1,61 @@
 import React,{Component} from 'react';
 import axios from 'axios';
+import {Dropdown} from 'react-bootstrap';
 
-
-export default class Second extends React.Component {
+export default class Second extends Component {
   constructor(props){
     super(props);
     this.state = {
+      categories : []
     }
   }
 
-
+componentDidMount(){
+  this.secondFunction();
+}
 
 secondFunction = () => {
    //go to npm axios and it will show you how to write it
    axios.get('https://api.chucknorris.io/jokes/categories')
      .then(response => {
-       console.log('im second:', response);
+       console.log('im second:', response.data);
+       this.setState({categories : response.data })
      })
-     .catch(function(error){
+     .catch(error => {
        console.log('error:', error);
      })
 }
 
+
+retrieveCategory = (item) => {
+  console.log('category clicked:', item);
+  axios.get(`https://api.chucknorris.io/jokes/random?category=${item}`)
+  .then(res =>(
+    console.log('response by category', res)
+  ))
+  .catch(err => {
+    console.log('err in category', err);
+  })
+}
+
   render() {
-    console.log('this is the initial state of our randomjoke:', this.state.randomjoke);
+    console.log('categories:', this.state.categories);
+    let categories = this.state.categories;
     return (
       <div>
-        <div>
-          <button className="secondB" onClick={this.secondFunction}>YES</button>
-          <button className="thirdB" onClick={this.thirdFunction}>NO</button>
-        </div>
-      </div>
+        <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Dropdown Button
+        </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+          {categories.map(item => {
+            return <span><Dropdown.Item onClick={this.retrieveCategory.bind(this, item)}>{item}</Dropdown.Item> <br/></span>
+          })
+        }
+        </Dropdown.Menu>
+     </Dropdown>
+     </div>
     )
   }
 }
